0d8e2abcd8b3839b5be84cb96dff014d507df180
Alan Campora <alan.campora@gmail.com>
2019-03-16 15:34:54 -0300

    refactor(*): getting youtube videos at building time

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 #frontend topics 
 Welcome to the front-end topics code!. I hope everyone can collaborate with me to create a front-end community. 
+
+
---
+++ gatsby-node.js
@@ -0,0 +1,57 @@
+// const {createFilePath} = require('gatsby-source-filesystem');
+// const path = require('path');
+const ypi = require("youtube-channel-videos");
+const secrets = require('./secrets');
+const crypto = require(`crypto`);
+
+const getVideos = function(key, channelId) {
+  return new Promise(resolve => {
+    ypi.channelVideos(
+      key,
+      channelId,
+      items => resolve(items),
+    );
+  });
+};
+
+exports.sourceNodes = async ({
+  actions,
+  createContentDigest,
+  createNodeId,
+}) => {
+  const { createNode } = actions;
+
+  const videos = await getVideos(secrets.key, secrets.channelId);
+
+  const ytNode = {
+    id: "ytNode",
+    parent: null,
+    children: videos.map(video => createNodeId(video.id.videoId)),
+    internal: {
+      type: "ytNode",
+    },
+  };
+  const contentDigest = crypto
+    .createHash(`md5`)
+    .update(JSON.stringify(ytNode))
+    .digest(`hex`);
+
+  ytNode.internal.contentDigest = contentDigest;
+  createNode(ytNode);
+
+  videos.forEach(video => {
+    const nodeMetadata = {
+      id: createNodeId(video.id.videoId),
+      parent: "ytNode",
+      snippet: video.snippet,
+      children: [],
+      internal: {
+        type: "ytVideo",
+        contentDigest: createContentDigest(video),
+      },
+    };
+
+    const node = Object.assign({}, video, nodeMetadata);
+    createNode(node);
+  });
+};
