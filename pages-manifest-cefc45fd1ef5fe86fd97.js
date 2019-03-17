68c96a1240fb6672a50a7f7f45d6f196a0b5a49e
Alan Campora <alan.campora@gmail.com>
2019-03-17 14:18:17 -0300

    Updates

---
+++ pages-manifest-cefc45fd1ef5fe86fd97.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{137:function(a){a.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E",index:"694/path---index-6a9-nhkry9dDOdrgwUSQGVr8WJyhs"}}}}]);
+//# sourceMappingURL=pages-manifest-cefc45fd1ef5fe86fd97.js.map
